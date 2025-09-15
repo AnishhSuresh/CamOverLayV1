5be576efb6621f465c042b7d1a8c617c6dfb0dcf
Anishh Suresh <anishhsuresh@vt.edu>
2025-09-14 23:52:26 -0400

    Initial Commit

---
+++ CamOverLayV1/CamOverLayV1App.swift
@@ -0,0 +1,17 @@
+//
+//  CamOverLayV1App.swift
+//  CamOverLayV1
+//
+//  Created by Anishh Suresh on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CamOverLayV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CamOverLayV1Tests/CamOverLayV1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  CamOverLayV1Tests.swift
+//  CamOverLayV1Tests
+//
+//  Created by Anishh Suresh on 9/14/25.
+//
+
+import Testing
+@testable import CamOverLayV1
+
+struct CamOverLayV1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
